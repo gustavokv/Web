2505a00c40f48ef5204700e8f92ca122e063be56
Gustavo Volobueff <volobueffgustavo@gmail.com>
2024-10-29 18:19:47 -0300

    questao1 done

--- desafio-1.1/questao1.js
+++ desafio-1.1/questao1.js
@@ -1,3 +1,58 @@
 class Vertice{
+    #x;
+    #y;
+
+    constructor(x, y){
+        this._x = x;
+        this._y = y;
+    }
     
+    get x(){
+        return this._x;
+    }
+
+    get y(){
+        return this._y;
+    }
+
+    getDistancia(v){
+        return Math.sqrt((v.x - this._x) ** 2 + (v.y - this._y) ** 2);
+    }
+
+    move(nx, ny){
+        this._x = nx;
+        this._y = ny;
+    }
+
+    equals(v){
+        if(v.x == this._x && v.y == this._y)
+            return true;
+
+        return false;
+    }
 }
+
+export default Vertice;
+
+let x,y;
+let V = [3];
+var prompt = require('prompt-sync')();
+
+/* Preencher o array de vértices */
+for(let i=0;i<3;i++){
+    x = prompt('x' + Number(i+1) + ' ');
+    y = prompt('y' + Number(i+1) + ' ');
+
+    V[i] = new Vertice(x,y);
+}
+
+for (const i in V) {
+    console.log(V[i].x, V[i].y);
+}
+
+/* Testes dos métodos */
+console.log(V[0].getDistancia(V[1]));
+console.log(V[0].equals(V[1]));
+console.log(V[1].equals(V[2]));
+V[0].move(10, 15);
+console.log(V[0].x, V[0].y);
---
+++ desafio-1.1/questao2.js
@@ -0,0 +1,6 @@
+import Vertice from "./questao1.js";
+
+let a = new Vertice(1,2);
+
+console.log(a.x);
+
